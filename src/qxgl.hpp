831f59e8d4b01e106066f0926297649de0fbbf98
Heric da Silva Cruz <hericsilvaho@gmail.com>
2025-06-12 11:28:36 -0300

    adicionando texture_loader a qxgl

--- src/qxgl.hpp
+++ src/qxgl.hpp
@@ -1,9 +1,15 @@
 #ifndef QXGL_HPP
-  // Core matemático
-  #include "core/vector3.hpp"
+#define QXGL_HPP
 
-  // Câmeras
-  #include "core/camera.hpp"
-  #include "core/game_camera.hpp"
-  #include "core/orbit_camera.hpp"
+// Core matemático
+#include "core/vector3.hpp"
+
+// Câmeras
+#include "core/camera.hpp"
+#include "core/game_camera.hpp"
+#include "core/orbit_camera.hpp"
+
+// Gerênciamento de Assets
+#include "assets_manager/texture_loader.hpp"
+
 #endif  // !QXGL_HPP

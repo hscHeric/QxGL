8670417112f68a4d02d9eff8dbbab63ba2849689
Heric da Silva Cruz <hericsilvaho@gmail.com>
2025-06-23 10:28:39 -0300

    adicionando qxgl.hpp

---
+++ src/qxgl.hpp
@@ -0,0 +1,29 @@
+#ifndef QXGL_HPP
+#define QXGL_HPP
+
+/**
+ * @file qxgl.hpp
+ * @brief Header principal da biblioteca QXGL.
+ *
+ * Inclua este arquivo para ter acesso a todas as funcionalidades
+ * da biblioteca QXGL com um único #include.
+ */
+// Utilitários e Estruturas de Dados
+#include "Transform.h"
+#include "Vetor3D.h"
+
+// Câmera
+#include "Camera.h"
+#include "CameraDistante.h"
+#include "CameraJogo.h"
+
+// Renderização e Desenho
+#include "Desenha.h"
+#include "Model3D.h"
+#include "OpenTextures.h"
+
+// GUI e Gerenciamento de Janela
+#include "extra.h"
+#include "gui.h"
+
+#endif  // !QXGL_HPP

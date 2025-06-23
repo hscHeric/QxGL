df8ec54a68064979e4160d797118823960a847da
Heric da Silva Cruz <hericsilvaho@gmail.com>
2025-06-23 12:44:49 -0300

    atualizando transformação

--- src/transform.cpp
+++ src/transform.cpp
@@ -1,46 +1,56 @@
 #include "transform.hpp"
 
-void Transform::apply() {
-  glTranslatef( this->pos.x, this->pos.y, this->pos.z );
-  glRotatef( this->rot.x, 1, 0, 0 );
-  glRotatef( this->rot.y, 0, 1, 0 );
-  glRotatef( this->rot.z, 0, 0, 1 );
-  if ( this->showLocalOrigin )
-    GUI::drawOrigin( 0.5 );
-  glScalef( this->esc.x, this->esc.y, this->esc.z );
-}
+#include "gui.hpp"
 
-void Transform::apply2D() {
-  glTranslatef( this->pos.x, this->pos.y, 0.0 );
-  glRotatef( this->rot.z, 0, 0, 1 );
-  if ( this->showLocalOrigin )
-    GUI::drawOrigin( 0.5 );
-  glScalef( this->esc.x, this->esc.y, 1.0 );
-}
+#include <GL/gl.h>
 
-void Transform::updateByMouse() {
-  this->pos.x += 0.05 * glutGUI::drx;
-  this->pos.y += 0.05 * glutGUI::dry;
-  this->pos.z += 0.05 * glutGUI::dlry;
-  this->rot.x += 0.25 * glutGUI::dly;
-  this->rot.y += 0.25 * glutGUI::dlx;
-  this->rot.z += 0.25 * glutGUI::dlrx;
-  this->esc.x += 0.25 * glutGUI::dmx;
-  this->esc.y += 0.25 * glutGUI::dmy;
-  this->esc.z += 0.25 * glutGUI::dlmy;
-}
+namespace qxgl {
+  void Transform::apply() const {
+    glTranslatef( position.x, position.y, position.z );
 
-void Transform::updateByMouse2D() {
-  this->pos.x += 0.05 * glutGUI::drx;
-  this->pos.y += 0.05 * glutGUI::dry;
-  this->rot.z += 0.25 * glutGUI::dlrx;
-  this->esc.x += 0.25 * glutGUI::dmx;
-  this->esc.y += 0.25 * glutGUI::dmy;
-}
+    glRotatef( rotation.x, 1.0F, 0.0F, 0.0F );
+    glRotatef( rotation.y, 0.0F, 1.0F, 0.0F );
+    glRotatef( rotation.z, 0.0F, 0.0F, 1.0F );
 
-void Transform::reset() {
-  this->pos             = qxgl::Vetor3D( 0.0, 0.0, 0.0 );
-  this->rot             = qxgl::Vetor3D( 0.0, 0.0, 0.0 );
-  this->esc             = qxgl::Vetor3D( 1.0, 1.0, 1.0 );
-  this->showLocalOrigin = true;
-}
+    if ( show_local_origin ) {
+      GUI::drawOrigin( 0.5 );
+    }
+
+    glScalef( scale.x, scale.y, scale.z );
+  }
+
+  void Transform::apply_2d() const {
+    glTranslatef( position.x, position.y, 0.0F );
+    glRotatef( rotation.z, 0.0F, 0.0F, 1.0F );
+
+    if ( show_local_origin ) {
+      GUI::drawOrigin( 0.5 );
+    }
+
+    glScalef( scale.x, scale.y, 1.0F );
+  }
+
+  void Transform::update_by_mouse() {
+    const Vetor3D POS_DELTA( glutGUI::drx, glutGUI::dry, glutGUI::dlry );
+    const Vetor3D ROT_DELTA( glutGUI::dly, glutGUI::dlx, glutGUI::dlrx );
+    const Vetor3D SCALE_DELTA( glutGUI::dmx, glutGUI::dmy, glutGUI::dlmy );
+
+    position += POS_DELTA * 0.05F;
+    rotation += ROT_DELTA * 0.25F;
+    scale += SCALE_DELTA * 0.25F;
+  }
+
+  void Transform::update_by_mouse_2d() {
+    position.x += glutGUI::drx * 0.05F;
+    position.y += glutGUI::dry * 0.05F;
+
+    rotation.z += glutGUI::dlrx * 0.25F;
+
+    scale.x += glutGUI::dmx * 0.25F;
+    scale.y += glutGUI::dmy * 0.25F;
+  }
+
+  void Transform::reset() {
+    *this = Transform();
+  }
+}  // namespace qxgl
--- src/transform.hpp
+++ src/transform.hpp
@@ -1,15 +1,42 @@
-#include "gui.hpp"
+#ifndef QXGL_TRANSFORM_HPP
+#define QXGL_TRANSFORM_HPP
 
-class Transform {
-public:
-  qxgl::Vetor3D pos             = qxgl::Vetor3D( 0.0, 0.0, 0.0 );
-  qxgl::Vetor3D rot             = qxgl::Vetor3D( 0.0, 0.0, 0.0 );
-  qxgl::Vetor3D esc             = qxgl::Vetor3D( 1.0, 1.0, 1.0 );
-  bool          showLocalOrigin = true;
+#include "vector_3d.hpp"
 
-  void apply();
-  void apply2D();
-  void updateByMouse();
-  void updateByMouse2D();
-  void reset();
-};
+namespace qxgl {
+  class Transform {
+    qxgl::Vetor3D position{ 0.0F, 0.0F, 0.0F };
+    qxgl::Vetor3D rotation{ 0.0F, 0.0F, 0.0F };
+    qxgl::Vetor3D scale{ 1.0F, 1.0F, 1.0F };
+    bool          show_local_origin{ true };
+
+    /**
+     * @brief Aplica as transformações 3D (escala, rotação, translação) na pilha de matrizes do
+     * OpenGL.
+     * @details A ordem de aplicação é Escala -> Rotação -> Translação.
+     */
+    void apply() const;
+
+    /**
+     * @brief Aplica as transformações 2D no plano XY.
+     */
+    void apply_2d() const;
+
+    /**
+     * @brief Atualiza os parâmetros de transformação 3D com base na entrada do mouse.
+     */
+    void update_by_mouse();
+
+    /**
+     * @brief Atualiza os parâmetros de transformação 2D com base na entrada do mouse.
+     */
+    void update_by_mouse_2d();
+
+    /**
+     * @brief Reseta todos os parâmetros de transformação para seus valores padrão.
+     */
+    void reset();
+  };
+}  // namespace qxgl
+
+#endif  // !QXGL_TRANSFORM_HPP

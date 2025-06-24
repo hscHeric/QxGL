a1138d2e7947610a0fb1dfd20b03cfb090541316
Heric da Silva Cruz <hericsilvaho@gmail.com>
2025-06-24 17:55:51 -0300

    qxgl.hpp atualizada

--- src/qxgl.hpp
+++ src/qxgl.hpp
@@ -1,30 +1,16 @@
 #ifndef QXGL_HPP
 #define QXGL_HPP
 
-/**
- * @file qxgl.hpp
- * @brief Header principal da biblioteca QXGL.
- *
- * Inclua este arquivo para ter acesso a todas as funcionalidades
- * da biblioteca QXGL com um único #include.
- */
-// Utilitários e Estruturas de Dados
-#include "transform.hpp"
-#include "vector_3d.hpp"
-
-// Câmera
 #include "camera.hpp"
 #include "distant_camera.hpp"
+#include "draw.hpp"
 #include "game_camera.hpp"
-
-// Renderização e Desenho
-#include "draw.hpp"
 #include "models_loader.hpp"
+#include "state.hpp"
 #include "textures_loader.hpp"
-
-// GUI e Gerenciamento de Janela
-#include "draw.hpp"
-#include "extra.hpp"
-#include "gui.hpp"
+#include "toolkit.hpp"
+#include "transform.hpp"
+#include "vector_3d.hpp"
+#include "window.hpp"
 
 #endif  // !QXGL_HPP

e11fe56f53b6a61a4762521826e78d337006c9de
Heric da Silva Cruz <hericsilvaho@gmail.com>
2025-07-16 23:39:30 -0300

    adicinando qxgl.hpp

---
+++ src/qxgl.hpp
@@ -0,0 +1,15 @@
+#ifndef QXGL_HPP
+#define QXGL_HPP
+
+#include "Camera.h"
+#include "CameraDistante.h"
+#include "CameraJogo.h"
+#include "Desenha.h"
+#include "Model3D.h"
+#include "OpenTextures.h"
+#include "Transform.h"
+#include "Vetor3D.h"
+#include "extra.h"
+#include "gui.h"
+
+#endif  // !QXGL_HPP
